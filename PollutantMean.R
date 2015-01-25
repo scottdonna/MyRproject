518051b2dc60790cff36e9ab043f3700b39bf4c9
scottdonna <scottdonna@users.noreply.github.com>
2015-01-24 22:45:46 -0500

    Update PollutantMean.R
    Finished changes with updated comments

--- PollutantMean.R
+++ PollutantMean.R
@@ -1,10 +1,10 @@
-pollutantmean <- function(directory, pollutant, id) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
         files_list <- list.files(directory, full.names=TRUE)  #creates a list of files
         dat <- data.frame()  #creates an empty dataframe
-        for (i in 1:length(files_list)) {
+        for (i in id) {
                 #loops trhough the files, rbinding them together
                 dat <- rbind(dat, read.csv(files_list[i]))    
         }
-        dat_sub <- dat[which(dat[,"ID"] %in% id),]
+        dat_sub <- dat[which(dat[,"ID"] %in% id),] #subsets the data contained in the 'id' input
         mean(dat_sub[, pollutant], na.rm=TRUE)  #identifies the mean of pollutant and removes NA
 }
